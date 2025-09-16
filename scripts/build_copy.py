eaf6528c2842aa0ee61bcc6b7da4c81709c3751a
HDanke <dankethegamer@gmail.com>
2025-09-16 19:43:20 +0300

    Create build_copy.py

---
+++ scripts/build_copy.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from gmdkit.extra.globed import GlobedScript
+from gmdkit.models.object import ObjectList
+from gmdkit.mappings import prop_id
+
+folder = Path("lua_scripts")
+
+object_list = ObjectList()
+
+y = 15
+
+for file in folder.glob("*.lua"):
+  script_object = GlobedScript()
+  script_object.import_script(file)
+  script_object.prefix = file.stem
+  script_object[prop_id.y] = y = y-30
+  obj_list.append(script_object)
+
+object_list.to_file("object_string.txt", encoded=False)
